f200ec5983023ba67df4a530246f63bba1d1411d
Micah Hirsch <92609212+micah-hirsch@users.noreply.github.com>
2025-05-06 15:56:33 -0400

    setting up test of interp_filter

---
+++ tests/testthat/test_interp_filter.R
@@ -0,0 +1,20 @@
+ # Testing interp_filter function
+
+library(tidyverse)
+library(zoo)
+library(abind)
+library(signal)
+library(readr)
+library(here)
+
+test_that("interp_filter filters data", {
+  test_data <- load_tsv(here("tests", "sample_data", "PLURAL44_RP.tsv"))
+  n_time <- dim(test_data[[1]])[1]
+  n_sensors <- dim(test_data[[1]])[3]
+  ref_idx <- c(1, 2, 3)
+  
+  filtered <- interp_filter(test_data, ref_idx)
+  
+  expect_equal(dim(filtered), dim(test_data))
+  expect_true(sum(is.na(data)) == 0)
+})
