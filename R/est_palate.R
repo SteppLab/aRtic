0b61e558b26b52fdd1101f9dc303388bb1e388d8
Micah Hirsch <92609212+micah-hirsch@users.noreply.github.com>
2025-06-06 15:38:57 -0400

    saving progress

--- R/est_palate.R
+++ R/est_palate.R
@@ -9,7 +9,7 @@
 #' @param rotation A rotation matrix extracted from define_coord
 #' @param center A vector with a length of 3 representing the translation vector extracted from define_coord
 #' @return A 3D array of the rotated data from the data recording
-#' @import dplyr pracma correct_mov interp_filter
+#' @import dplyr pracma geometry correct_mov interp_filter
 #' @export
 #' 
 
@@ -25,43 +25,17 @@ est_palate <- function(data, ref_idx, pl_idx, rotation, center) {
   
   palate_trace <- corrected_palate[, 1:3, ]
   
-  ref_mean <- apply(palate_trace[, , ref_idx], c(2, 3), mean, na.rm = T)
-  s1 <- ref_mean[,1]
-  s2 <- ref_mean[,2]
-  s3 <- ref_mean[,3]
+  ref_mean <- apply(palate_trace[, , ref_idx], c(2, 3), mean, na.rm = T) |>
+    t()
   
-  s1_s2 <- s2 - s1
-  s1_s3 <- s3 - s1
-  norm_vec <- pracma::cross(s1_s2, s1_s3)
-  norm_vec <- norm_vec/sqrt(sum(norm_vec^2))
+  hull <- geometry::convhulln(ref_mean)
   
-  u <- s1_s2 / sqrt(sum(s1_s2^2))
-  v <- pracma::cross(norm_vec, u)
   
-  to_2d <- function(P) {
-    rel <- P - s1
-    c(sum(rel * u), sum(rel * v))
-  }
-  
-  tri_2d <- t(sapply(list(s1, s2, s3), to_2d))
-  
-  stylus <- data[ , , 8]  # stylus data
-  inside <- logical(nrow(stylus))
-  
-  for (t in 1:nrow(stylus)) {
-    pt <- stylus[t, ]
-    if (any(is.na(pt))) next
     
-    pt_2d <- to_2d(pt)
-    
-    
-    
-  }
-  
-  stylus_filtered <- stylus
-  stylus_filtered[!inside, ] <- NA
-  
-  data_filtered <- data
-  data_filtered[ , , 8] <- stylus_filtered
-  
+}
+
+
+.project_to_plane_2d <- function(P) {
+  rel <- P - A
+  c(sum(rel * u), sum(rel * v))
 }
