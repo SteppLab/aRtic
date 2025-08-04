57707abfd5c28d647dea1aa7b08b4c15225c8980
Micah Hirsch <92609212+micah-hirsch@users.noreply.github.com>
2025-08-04 14:08:26 -0400

    refitting splines to data

--- tests/informal_tests/test_est_palate_informal.R
+++ tests/informal_tests/test_est_palate_informal.R
@@ -17,9 +17,9 @@ source(".\\R\\est_palate.R")
 source(".\\R\\norm_vec.R")
 source(".\\R\\center.R")
 
-bite_data <- load_tsv("R:\\SteppLab3\\Projects\\Voice\\SAKE\\artic_recordings\\PLURAL33\\Plural33_BitePlane.tsv")
+bite_data <- load_tsv(here("tests", "sample_data", "PLURAL02_BitePlane.tsv"))
 
-ref_idx <- c(1,2,4)
+ref_idx <- c(1,2,3)
 bp_idx <- c(5,6,7)
 pl_idx <- 8
 
@@ -27,13 +27,13 @@ bite_data_3d <- bite_data[[1]]
 
 rotated <- define_coord(bite_data_3d, ref_idx, bp_idx)
 
-rotated_plane <- rotated[[1]]
+coord <- rotated[[1]]
 
-rotation <- rotated[[2]]
+base_rt <- rotated[[2]]
 
-center <- rotated[[3]]
+base_center <- rotated[[3]]
 
-data_palate <- load_tsv("R:\\SteppLab3\\Projects\\Voice\\SAKE\\artic_recordings\\PLURAL33\\PLURAL33_PalateTrace.tsv")
+data_palate <- load_tsv(here("tests", "sample_data", "PLURAL02_PalateTrace.tsv"))
 
 data <- data_palate[[1]]
 
@@ -59,3 +59,44 @@ palate_df <- as.data.frame(palate_df)
 plot_ly(palate_df, x = ~X, y = ~Y, z = ~Z, color = ~Sensor,
         type = "scatter3d", mode = "lines") 
 
+tri_faces <- hull_faces$triang
+
+hull_faces <- as.data.frame(hull_faces)
+
+num_faces <- nrow(hull_faces)
+i <- seq(0, by = 3, length.out = num_faces)
+j <- i + 1
+k <- i + 2
+
+plot_ly(spline_df, x = ~X, y = ~Y, z = ~Z, type = "scatter3d",
+        mode = "lines+markers",
+              marker = list(size = 2))
+
+td_hull <- spline[, 1:2]
+
+hull <- concaveman(td_hull, concavity = 2)
+
+plot(hull, type = "l")
+
+plot_ly() %>%
+  add_trace(
+    type = "scatter3d",
+    mode = "lines",
+    x = spline[, 1],
+    y = spline[, 2],
+    z = spline[, 3],
+    line = list(color = 'red', width = 4),
+    name = "Smoothed Spline"
+  )
+
+plot_ly() %>%
+  add_trace(
+    type = "scatter",
+    mode = "markers",
+    x = palate_coords[, 1],
+    y = palate_coords[, 2],
+    marker = list(size = 2),
+    name = "Original Data"
+  ) |>
+  add_trace(x = hull[,1], y = hull[,2], 
+            type = 'scatter', mode = 'lines', line = list(color = 'red', width = 4))
